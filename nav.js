072dbcb721e1615706e73e478c08931003d6b10a
Richard <richard_stephens@hotmail.co.uk>
2023-06-19 14:24:49 +0100

    added navbar code

---
+++ nav.js
@@ -0,0 +1,26 @@
+console.log("work");
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav_active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+      }
+    });
+    //Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
